0df84beeb0d7b8264506111e5c4094dfffb9d191
carlos <chlouzada@gmail.com>
2022-11-04 10:13:43 -0300

    priority to pnpm on install script

--- script.sh
+++ script.sh
@@ -10,18 +10,23 @@ if [ -e $PACKAGE_NAME ]; then
 fi
 
 # git
-printf '\n'
 git clone https://github.com/chlouzada/ts-lib $PACKAGE_NAME
 rm -rf $PACKAGE_NAME/.git
 git init $PACKAGE_NAME
 git -C $PACKAGE_NAME branch -m main
 
 # package.json
-printf '\nSetup package.json\n'
 PACKAGE_JSON=$(cat $PACKAGE_NAME/package.json)
 PACKAGE_JSON=$(echo $PACKAGE_JSON | jq ".name = \"$PACKAGE_NAME\"")
 echo $PACKAGE_JSON > $PACKAGE_NAME/package.json
 
-# npm
-printf '\n Installing dependencies\n'
-npm install --prefix $PACKAGE_NAME
+# install dependencies
+if ! command -v pnpm &> /dev/null
+then
+  echo "pnpm could not be found"
+  echo "Using npm"
+  rm $PACKAGE_NAME/pnpm-lock.yaml
+  npm install --prefix $PACKAGE_NAME
+  exit
+fi
+pnpm install --prefix $PACKAGE_NAME
